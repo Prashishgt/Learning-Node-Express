f3f7485e226634cdcc01dbd6f7f21af925103639
Prashishgt <prashish24@gmail.com>
2023-11-03 19:53:58 +0545

    completed express

--- 02-express/app.js
+++ 02-express/app.js
@@ -1,25 +1,10 @@
 const express = require("express");
-const path = require("path");
 const app = express();
-
-app.use(express.static("./public"));
-
+const { products } = require("./data");
 app.get("/", (req, res) => {
-  res.sendFile(path.resolve(__dirname, "./navbar-app/index.html"));
-});
-
-app.all("*", (req, res) => {
-  res.status(400).send("Resource not found");
+  res.json(products);
 });
 
 app.listen(5000, () => {
-  console.log(`Server is listening on port 5000...`);
+  console.log("Server is listening on port 5000....");
 });
-
-// app.get
-// app.post
-// app.delete
-// app.put
-// app.all
-// app.use
-// app.listen
