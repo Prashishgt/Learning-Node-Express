6e7c6aa8b038735096f315b289b9c474bbb166f9
Prashishgt <prashish24@gmail.com>
2023-11-04 20:43:47 +0545

    Learning about schema of mongodb

--- 03-task-manager/starter/app.js
+++ 03-task-manager/starter/app.js
@@ -1,1 +1,29 @@
-console.log('Task Manager App')
+require("./db/connect");
+const express = require("express");
+const app = express();
+const tasks = require("./routes/task");
+const connectDB = require("./db/connect");
+require("dotenv").config();
+
+// middleware
+app.use(express.json());
+
+// routes
+app.get("/hello", (req, res) => {
+  res.send("Task Manager Task");
+});
+
+app.use("/api/v1/tasks", tasks);
+
+const port = 3000;
+
+const start = async () => {
+  try {
+    await connectDB(process.env.MONGO_URL);
+    app.listen(port, console.log(`Server is listening on port ${port}...`));
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+start();
