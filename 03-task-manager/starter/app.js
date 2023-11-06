577917c9fa2867957b21bb331c47a4d09a4c40a0
Prashishgt <prashish24@gmail.com>
2023-11-06 15:33:34 +0545

    understanding the difference between put and update

--- 03-task-manager/starter/app.js
+++ 03-task-manager/starter/app.js
@@ -6,12 +6,8 @@ const connectDB = require("./db/connect");
 require("dotenv").config();
 
 // middleware
+app.use(express.static("./public"));
 app.use(express.json());
-
-// routes
-app.get("/hello", (req, res) => {
-  res.send("Task Manager Task");
-});
 
 app.use("/api/v1/tasks", tasks);
 
