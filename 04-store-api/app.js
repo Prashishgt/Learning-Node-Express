688a5b033406fa39f9f381fd2ecbc7f12e4bf898
Prashish G.T. <prashish24@gmail.com>
2023-11-07 16:30:25 +0545

    Authentication still remaning for product

--- 04-store-api/app.js
+++ 04-store-api/app.js
@@ -1,1 +1,39 @@
-console.log('04 Store API')
+require("dotenv").config();
+require("express-async-errors");
+
+const connectDB = require("./db/connect");
+const productRouter = require("./routes/products");
+
+const express = require("express");
+const app = express();
+
+const notFoundMiddleware = require("./middleware/not-found");
+const errorMiddleware = require("./middleware/error-handler");
+
+// middleware
+app.use(express.json());
+
+// routes
+app.get("/", (req, res) => {
+  res.send(`<h1>Store API</h1><a href="/api/v1/product">Products route</a>`);
+});
+
+app.use("/api/v1/products", productRouter);
+// products routes
+
+const port = process.env.PORT || 3000;
+
+app.use(notFoundMiddleware);
+app.use(errorMiddleware);
+
+const start = async () => {
+  try {
+    // connect db
+    await connectDB(process.env.MONGO_URL);
+    app.listen(port, console.log(`Server is listening at ${port}`));
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+start();
